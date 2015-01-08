aaf9f9fb33c07c35f2395c01d1ade9c51784f538
mwaas <francismwangi152@gmail.com>
2015-01-08 13:49:27 +0300

    changed the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-waffle-session',
-    version='0.1.1',
+    version='0.1.2',
     description='A feature flipper for Django.',
     long_description=open('README.rst').read(),
     author='Francis Mwangi',
