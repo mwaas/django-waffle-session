de5e343dfa533addae434fa18fe71edad3be5e18
Collin <collinmusonye@gmail.com>
2019-09-26 15:12:27 +0300

    Revert "Minimal import export int (#3)"
    This reverts commit e983e7d125a4d64dbb411a8f7d937a858f152c4b. - Feature was abandoned midway

--- waffle/admin.py
+++ waffle/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from import_export.admin import ImportExportModelAdmin
-from resources import VerifiedUserResource
+
 from waffle.models import Flag, Sample, Switch, VerifiedUser
 
 
@@ -29,11 +28,10 @@ class FlagAdmin(admin.ModelAdmin):
     raw_id_fields = ('users', 'groups')
     ordering = ('-id',)
 
-class VerifiedUserAdmin(ImportExportModelAdmin):
+class VerifiedUserAdmin(admin.ModelAdmin):
     list_display = ('phone_number', 'feature')
     list_filter = ('feature', )
     search_fields = ('phone_number', )
-    resource_class = VerifiedUserResource
 
 
 
