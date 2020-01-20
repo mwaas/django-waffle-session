061a4099faf46d25beae541c28fef9d3def9c26e
Emmanuel Telewa <emmanuel.telewa@jumo.world>
2020-01-20 12:54:12 +0300

    new version for lnp

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-waffle-session',
-    version='0.2.8b',
+    version='0.2.8',
     description='A feature flipper for Django.',
     long_description=open('README.rst').read(),
     author='Francis Mwangi',
